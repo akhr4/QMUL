571c4c64259803a32fa3f79043e04ebdad79714d
akhr4 <46759253+akhr4@users.noreply.github.com>
2019-10-23 21:22:02 +0100

    Update L2L3quiz.java

--- L2L3quiz.java
+++ L2L3quiz.java
@@ -19,9 +19,10 @@ class Main{
     }
     //introduction method that asks the user's name and outputs a small message as an introduction 
     public static int age(){
-        int age = inputInt("Please enter your age so I know how to address you");
+        int age = inputInt("Please enter your age so I can determine the difficulty level of the quiz.");
         return age;
     }
+    //Basic menu that only accepts 1,2,3 as an answer and nothing else. 
     public static void menu(){
         int choice = 0; 
         while (choice != 1){
@@ -40,17 +41,18 @@ class Main{
     return;
 
     }
-    
+    //input method that takes in string inputs 
     public static String input(String message){
     System.out.println(message);
     Scanner scanner = new Scanner(System.in);
     return scanner.nextLine();
     }
-
+    //input methods that take integer inputs by utilising parsing and the previous string input method 
     public static int inputInt(String message){
         return Integer.parseInt(input(message));
         
     }
+    //method that prints called print for ease of typing 
     public static void print(String message){
       System.out.println(message);
     }
