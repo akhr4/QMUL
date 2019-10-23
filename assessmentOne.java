cc779b53cca535304269482a8fcaa025c2498279
akhr4 <46759253+akhr4@users.noreply.github.com>
2019-10-23 10:23:11 +0100

    Create assessmentOne.java

---
+++ assessmentOne.java
@@ -0,0 +1,33 @@
+/*Assessment 1 - Output 
+Date: Mon 23rd Sept 2019
+Author: Ayushi Khetani
+*/
+
+class assessmentOne{ //Opening the class 
+	public static void main(String args[]){ //main method 
+	letterA(); //Printing letter A method
+	System.out.println(); //Printing a blank line to seperate the methods 
+	System.out.println(); //Printing a blank line to seperate the methods
+	letterK(); //Printing letter K method 
+	System.exit(0);
+	}
+	
+	public static void letterA(){
+		//Printing A in a pattern of A using A's
+		System.out.println("AAAAA");
+		System.out.println("A   A");
+		System.out.println("A   A");
+		System.out.println("AAAAA");
+		System.out.println("A   A");
+		System.out.println("A   A");
+	}
+	
+	public static void letterK(){
+		//Printing K in a pattern of K using K's
+		System.out.println("K   K");
+		System.out.println("K K");
+		System.out.println("KK");
+		System.out.println("K K");
+		System.out.println("K   K");
+	}
+}
