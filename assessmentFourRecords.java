2b9c50d782580dab80940a9682e9ad17d6819d18
akhr4 <46759253+akhr4@users.noreply.github.com>
2019-10-23 10:25:47 +0100

    Create assessmentFourRecords.java

---
+++ assessmentFourRecords.java
@@ -0,0 +1,90 @@
+/*Assessment 4: Records
+Date Done: 28th Sept 2018
+Author: Ayushi Khetani
+*/
+
+import java.util.Scanner;
+
+class station{  //Record class with 3 fields of station's name,cost and toilet there
+	String stationName; //Assigning data types to record attributes 
+	Boolean toilet;
+	Integer cost;
+}
+
+class assessmentFourRecords{
+	public static void main(String args[]){ //Main method
+		//Creating records for each station 
+		station stepneyGreen = new station();
+		stepneyGreen.stationName = "stepney green";
+		stepneyGreen.toilet = false;
+		stepneyGreen.cost = 0;
+
+		station greenPark = new station();
+		greenPark.stationName = "green park";
+		greenPark.toilet = true;
+		greenPark.cost = 30;
+
+		station kingsCross = new station();
+		kingsCross.stationName = "kings cross";
+		kingsCross.toilet = true;
+		kingsCross.cost = 10;
+
+		station oxfordCircus = new station();
+		oxfordCircus.stationName = "oxford circus";
+		oxfordCircus.toilet = false;
+		oxfordCircus.cost =0;
+		//Var string stationSearched which is being stored here from userInput method 
+		String stationSearched = userInput(); 
+		//Runs the validator method which decides what to do with user's input using correct args 
+		validator(stationSearched, stepneyGreen, greenPark, kingsCross, oxfordCircus);
+		return;
+	}
+
+	public static String userInput(){  //Method taking in user's input
+	Scanner scanner =  new Scanner(System.in);
+	System.out.println("What station do you need to know about?"); //Displays this to the user
+	String stationSearched = scanner.nextLine().toLowerCase(); //Taking the user's input and storing to station searched as well as converting user's string to lower case
+	return stationSearched; // returns stationSearched variable to main
+	}
+
+	public static void validator(String stationSearched, station stepneyGreen, station greenPark, station kingsCross, station oxfordCircus){ //Validating method
+	//Conditionals
+	String unavailable = " station currently does not have a toilet in place.";
+	if (stationSearched.contains("green park")){
+		if (greenPark.toilet){
+		System.out.println(greenPark.stationName + " has a toilet costing " + greenPark.cost + "p");
+		}
+		else{
+			System.out.println(greenPark.stationName + unavailable);
+		}
+	}
+	else if (stationSearched.contains("stepney green")){
+		if (stepneyGreen.toilet){
+		System.out.println(stepneyGreen.stationName + " has a toilet costing " + stepneyGreen.cost + "p");
+		}
+		else{
+			System.out.println(stepneyGreen.stationName + unavailable);
+		}
+	}
+	else if (stationSearched.contains("kings cross")){
+		if (kingsCross.toilet){
+		System.out.println(kingsCross.stationName + " has a toilet costing " + kingsCross.cost + "p");
+		}
+		else{
+			System.out.println(kingsCross.stationName + unavailable);
+		}
+	}
+	else if (stationSearched.contains("oxford circus")){
+		if (oxfordCircus.toilet){
+		System.out.println(oxfordCircus.stationName + " has a toilet costing " + oxfordCircus.cost + "p");
+		}
+		else{
+			System.out.println(oxfordCircus.stationName + unavailable);
+		}
+	}
+	else{
+		System.out.println("Is " + stationSearched +" a London underground station? Check your spelling."); //If the input is not understood, it takes the user back to main method to try again
+		assessmentFourRecords.main(null); //Take it back to main with incorect input
+	}
+	}
+}
