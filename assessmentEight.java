7c2925fcb71119fae00743cc97bd0848b529101b
akhr4 <46759253+akhr4@users.noreply.github.com>
2019-10-23 11:04:49 +0100

    Create assessmentEight.java

---
+++ assessmentEight.java
@@ -0,0 +1,20 @@
+  import java.util.Scanner;
+  class Main {
+    public static void main(String[] args){
+        
+        
+    }
+    public static String input(String message){
+        System.out.println(message);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+    
+    
+    
+}
+
+class Car{
+    String make;
+    boolean electric;
+}
