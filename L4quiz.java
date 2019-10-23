c12238dc7c284bcbe4936c6581cb9d5b5c072cc2
akhr4 <46759253+akhr4@users.noreply.github.com>
2019-10-23 17:44:41 +0100

    Update L4quiz.java

--- L4quiz.java
+++ L4quiz.java
@@ -9,19 +9,23 @@ import java.util.Scanner; //For user input in console
 class Main{
     public static void main(String[] args){ 
         print("Welcome to the impossible quiz. You need to make sure you answer each question correctly");
+        String name = name();
         int age = age();
-        String title = titleAddressed(age);
-        String name = input("What is your name?");
-        print("Hello, " + name + ". It is nice to meet you!");
         menu();
         System.exit(0);
         
     }
     //introduction method that asks the user's name and outputs a small message as an introduction 
     public static int age(){
-        int age = inputInt("Please enter your age so I know how to address you");
+        int age = inputInt("Please enter your age:");
         return age;
     }
+    
+    public static String name(){
+        String name = input("Please enter your name:");
+        return name;
+    }
+    
     public static void menu(){
         int choice = 0; 
         while (choice != 1){
@@ -53,4 +57,3 @@ class Main{
       System.out.println(message);
     }
 }
-
