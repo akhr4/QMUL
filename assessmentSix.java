a7b9992362f9d1a12493639d69f1522e26dfefa8
akhr4 <46759253+akhr4@users.noreply.github.com>
2019-10-23 10:26:20 +0100

    Create assessmentSix.java

---
+++ assessmentSix.java
@@ -0,0 +1,42 @@
+/*Assessment 6: Arrays 
+Date Done: 15th Oct 2018
+Author: Ayushi Khetani
+*/
+import java.util.Scanner;
+ 
+class assessmentSix{
+	public static void main(String args[]){
+		//Initialising array of data type String called films with a size of 5
+		String[] films = new String[5];
+		//Assigning indexes of the array a data element (film)
+		films[0] = "Blinded By The Light";
+		films[1] = "Captain Marvel";
+		films[2] = "IT: Chapter 2";
+		films[3] = "The Martian";
+		films[4] = "Hidden Figures";
+		int[] filmProfit = new int[5]; //Creating profit array size 5 only taking integer 
+		for (int i=0; i< films.length; i++){ //For loop start at 0, up to 5 incrementing by 1 
+			System.out.println("Film: " + films[i]); //Prints film at each index 
+			int inputProfit = userIntPut("How much profit did " + films[i] + " take in millions of pounds"); //asks user for number using method made and storing it to inputProfit as an integer
+			filmProfit[i] = inputProfit; //Adding the user's input to premade array to store 
+			}
+			System.out.println("The films and the amount they made were:");
+		for (int j=0; j<films.length; j++){ //Loop listing the elements from the array showing the end profits 
+			System.out.println(films[j] + " made £" + (filmProfit[j]*1000000)); 
+		}
+		int sum = filmProfit[0] + filmProfit[1] + filmProfit[2] + filmProfit[3] + filmProfit[4];
+		System.out.println("Together these " + films.length + " took " + sum + " million pounds at the box office");
+		return;
+	}
+	//Method to input a string 
+	public static String userStringPut(String prompt){
+		System.out.print(prompt);
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextLine();
+	}
+	//Method to input an integer which uses the input string method 
+	public static int userIntPut(String prompt){
+		//System.out.print(prompt);
+		return Integer.parseInt(userStringPut(prompt));
+	}
+} 
