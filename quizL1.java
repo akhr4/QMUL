01f1a7f4f9ded035d6d7873188ff32b88840980f
akhr4 <46759253+akhr4@users.noreply.github.com>
2019-10-23 11:51:10 +0100

    Update quizL1.java

--- quizL1.java
+++ quizL1.java
@@ -3,15 +3,20 @@ Project: Impossible Quiz
 Level: 1
 */
 
-import java.swing.* ;  //API for GUI
 import java.util.Scanner; //For user input in console 
 
 //Main class 
-class Quiz{
+class Main{
     public static void main(String[] args){ 
         print("Welcome to the impossible quiz. You need to make sure you answer each question correctly");
+        introduction();
+        
+    }
+    //introduction method that asks the user's name and outputs a small message as an introduction 
+    public static void introduction(){
         String name = input("What is your name?");
         print("Hello, " + name + ". It is nice to meet you!");
+        return;
     }
     
     public static String input(String message){
@@ -22,5 +27,5 @@ class Quiz{
 
     public static void print(String message){
       System.out.println(message);
-      }
+    }
 }
