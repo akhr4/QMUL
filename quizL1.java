b0e66878d619ec40a65d3d31623f967da45de038
akhr4 <46759253+akhr4@users.noreply.github.com>
2019-10-28 11:19:07 +0000

    Update quizL1.java

--- quizL1.java
+++ quizL1.java
@@ -9,22 +9,24 @@ import java.util.Scanner; //For user input in console
 class Main{
     public static void main(String[] args){ 
         print("Welcome to the impossible quiz. You need to make sure you answer each question correctly");
-        introduction();
+        introduction(); //Go to the introduction method 
         
     }
     //introduction method that asks the user's name and outputs a small message as an introduction 
     public static void introduction(){
-        String name = input("What is your name?");
-        print("Hello, " + name + ". It is nice to meet you!");
+        String name = input("What is your name?"); //Takes input and stores it to name 
+        print("Hello, " + name + ". It is nice to meet you!"); //Prints the message using user's name 
         return;
     }
-    
+    //String input method that uses Scanner 
     public static String input(String message){
     System.out.println(message);
     Scanner scanner = new Scanner(System.in);
     return scanner.nextLine();
     }
 
+    
+    //Method to imple print messages so instead of using System.out.println , print can be used 
     public static void print(String message){
       System.out.println(message);
     }
