e81c8654e15b8f3facc7cf9d923b3b398fdeff86
akhr4 <46759253+akhr4@users.noreply.github.com>
2019-11-05 11:11:34 +0000

    Update assessmentEight.java

--- assessmentEight.java
+++ assessmentEight.java
@@ -1,20 +1,20 @@
-import java.util.Scanner; //for user input 
+import java.util.Scanner; /
     
-    //Main class
-  class Main {
+    //Main class creating the records of all the 3 cars and then running the method that contaisn the compatators
+class assessmentEight {
     public static void main(String[] args){
         Car triumph = new Car(); //New car record per car make 
         
-        triumph.setMake("triumph");
-        triumph.setElectric(false);
+        triumph = setMake(triumph, "triumph");
+        triumph = setElectric(triumph, false);
         
         Car miniE = new Car();
-        miniE.setMake("mini e"); //String make type 
-        miniE.setElectric(true); //Boolean if car is electric or not 
+        miniE = setMake(miniE,"mini e"); //String make type 
+        miniE = setElectric(miniE,true); //Boolean if car is electric or not 
         
         Car focus = new Car();
-        focus.setMake("focus");
-        focus.setElectric(false);
+        focus = setMake(focus,"focus");
+        focus = setElectric(focus, false);
         
         String carMake = input("What car do you need to know about? ").toLowerCase(); //Asking user for the ar make they want to find out About
         information(miniE, focus, triumph, carMake);
@@ -27,16 +27,16 @@ import java.util.Scanner; //for user input
     }
     
     public static void information(Car miniE, Car focus, Car triumph, String carMake){
-        if (carMake.equals(triumph.getMake())){ //If the user asks about a Triumph
-            if(triumph.getElectric()){ //If the triumph's electric status is true, 
+        if (carMake.equals(getMake(triumph))){ //If the user asks about a Triumph
+            if(getElectric(triumph)){ //If the triumph's electric status is true, 
                 System.out.println(carMake + " is electric"); //Tell the user the car is electric 
             }
             else{ //If the elctric status is false, 
                 System.out.println(carMake + " is not electric"); //Tell the user the car is not electric 
             }
         }
-        else if (carMake.equals(miniE.getMake())){
-            if(miniE.getElectric()){
+        else if (carMake.equals(getMake(miniE))){
+            if(getElectric(miniE)){
                 System.out.println(carMake + " is electric");
             }
             else{
@@ -44,8 +44,8 @@ import java.util.Scanner; //for user input
             }
             
         }
-        else if (carMake.equals(focus.getMake())){
-            if(focus.getElectric()){
+        else if (carMake.equals(getMake(focus))){
+            if(getElectric(focus)){
                 System.out.println(carMake + " is electric");
             }
             else{
@@ -58,28 +58,27 @@ import java.util.Scanner; //for user input
         return; //Defensive programming 
     }
     
+    public static String getMake(Car c){
+      return c.make;
+    }
+    
+    public static Boolean getElectric(Car c){
+      return c.electric;
+    }
+    
+    public static Car setMake(Car c, String make){
+      c.make = make;
+      return c;
+    }
+    
+    public static Car setElectric(Car c, boolean electric){
+      c.electric = electric;
+      return c;
+    }
 }
 
 class Car{
     //Car class with the 2 attributes 
     String make;
     boolean electric;
-    
-    //Setters
-    public void setMake(String make){
-        this.make = make;
-    }
-    
-    public void setElectric(Boolean electric){
-        this.electric = electric;
-    }
-    
-    //Getters
-    public String getMake(){
-        return make;
-    }
-    
-    public Boolean getElectric(){
-        return electric;
-    }
 }
