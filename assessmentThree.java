9bbfae9dc732fa24aa7b0fba5ac374ab11c9f86e
akhr4 <46759253+akhr4@users.noreply.github.com>
2019-11-28 20:01:40 +0000

    Update assessmentThree.java

--- assessmentThree.java
+++ assessmentThree.java
@@ -10,8 +10,9 @@ class assessmentThree{ //Opening the class
 	validation();
 	//System.exit(0)
 	}
-		//Method that allows for the user to input their choice 
-		public static String vendingInput(){
+	
+	//Method that allows for the user to input their choice 
+	public static String vendingInput(){
 		Scanner scanner = new Scanner(System.in); //Scanner to take user input
 		//Printing the vending machine menu!
 		System.out.println("Choose from A: Packet of Crisps, B: Health Bar, C: Chocolate Bar");
@@ -20,25 +21,25 @@ class assessmentThree{ //Opening the class
 	}
 	
 	public static void validation(){
-	String choice = vendingInput(); //Getting string user input from previous method
-	//The choices the user picks corresponds to vending menu 
-	String packetOfCrisps = "a";
-	String healthBar = "b";
-	String chocolateBar = "c";
-	//starting conditionals
-	if (packetOfCrisps.equals(choice)){ //If A is picked
-		System.out.println("The crisps costs 1.50.");
-	}
-	else if(healthBar.equals(choice)){ //If B is picked
-		System.out.println("The health bar costs 1.20.");
-	}
-	else if(chocolateBar.equals(choice)){ //If C is picked
-		System.out.println("The chocolate bar costs 2.00.");
-	}
-	else { //If it was not A,B,C then the console will tell the user that their option choice was invalid and needs to be picked again
-		System.out.println(choice + " is not an option in the vending machine. Please pick A B or C which are options you can actually get");
-		assessmentThree.main(null); //calling main menu method in order to loop the thing until the user gives proper answer
-	}
-	return;
+		String choice = vendingInput(); //Getting string user input from previous method
+		//The choices the user picks corresponds to vending menu 
+		String packetOfCrisps = "a";
+		String healthBar = "b";
+		String chocolateBar = "c";
+		//starting conditionals
+		if (packetOfCrisps.equals(choice)){ //If A is picked
+			System.out.println("The crisps costs 1.50.");
+		}
+		else if(healthBar.equals(choice)){ //If B is picked
+			System.out.println("The health bar costs 1.20.");
+		}
+		else if(chocolateBar.equals(choice)){ //If C is picked
+			System.out.println("The chocolate bar costs 2.00.");
+		}
+		else { //If it was not A,B,C then the console will tell the user that their option choice was invalid and needs to be picked again
+			System.out.println(choice + " is not an option in the vending machine. Please pick A B or C which are options you can actually get");
+			assessmentThree.main(null); //calling main menu method in order to loop the thing until the user gives proper answer
+		}
+		return;
 	}
 }
